88e3b993e4f887fa3f9a82acf6c9b364154cbed0
Quang Nguyen <quangn8888@gmail.com>
2023-07-07 11:43:43 -0700

    add users_per_channel

---
+++ Google_Analytics_360_data/models/users_per_channel.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table') }}
+
+SELECT
+    date,
+    channelGrouping,
+    SUM(newVisits) AS new_visits,
+    SUM(visits) AS visits
+FROM
+    {{source('raw', 'ga_data_raw') }}
+GROUP BY
+    date,
+    channelGrouping
+
