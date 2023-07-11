43a775b43033a6cf130b12d3d15a0b8468330e73
Quang Nguyen <quangn8888@gmail.com>
2023-07-11 09:56:30 -0700

    update

--- Google_Analytics_360_data/ga_data_pipeline.py
+++ Google_Analytics_360_data/ga_data_pipeline.py
@@ -16,3 +16,82 @@ GA_STORAGE = 'gs//ga_storage_dtc-project'
 gcp_credentials_block = GcpCredentials.load("dtc-de-gcp")
 
 @task(retries = 1, log_prints = True)
+def read_from_bp() -> pd.DataFrame:
+    """Task to load dta from BQ table to pandas dataframe
+        Data gathered by SQL query
+        Data load to cloud storage in parquet format.
+        
+        Args:
+            None.
+            
+        Return:
+            Dataframe.
+    """
+    query = """
+                    SELECT
+                        CAST(date AS DATE FORMAT 'YYYYMMDD) AS date,
+                        CAST(fullVisitorId AS STRING) AS fullVisitorId,
+                        totals.transactions AS transactions,
+                        device.browser AS browser,
+                        totals.timeOnSite AS timeOnSite,
+                        channelGrouping AS channelGrouping,
+                        geoNetwork.country AS country,
+                        totals.newVisits AS newVisits,
+                        totals.visits AS visits,
+                    FROM
+                        'bigquery-public-data.google_analytics_sample.ga_session_*'
+            """
+    return pd.read_gbq(query, project_id= PROJECT)
+
+
+@task(retries = 1, log_prints = True)
+def write_to_gcs(df: pd.DataFrame) -> None:
+    """"
+        Upload local parquet file to GCS
+        
+        Args:
+            Dataframe and path
+            
+        Returns:
+            None.
+    """
+    
+    gcs_block = GcsBucket.load("dtc-de-gcs")
+    return gcs_block.upload_from_dataframe(df = df, to_path= GA_PARQUET_FILENAME, serialization_format = 'parquet')
+
+    
+@task(retries = 1, log_prints = True)
+def extract_from_gcs(path) -> pd.DataFrame:
+    """
+        Download trip data from GCS
+
+        Args:
+            Dataframe and path
+            
+        Returns:
+            None.        
+        
+    """
+    print(path)
+    df = pd.read_parquet(path)
+    return df
+
+@task(retries = 1, log_prints = True)
+def write_to_bq(df: pd.DataFrame) -> None:
+    """
+        Write DataFrame to BigQuery
+        
+        Args:
+            DataFrame.
+            
+        Returns:
+            None.
+    """
+    
+    df['date'] = pd.to_datetime(df.date, format='%Y-%m-%dT%H:%M:%S.%f', errors='ignore')
+    df['date'] = df.date.dt.date
+    df['fullVisitorId'] = df.fullVisitorId.astype(str)
+    
+    df.to_gbq(
+        
+    )
