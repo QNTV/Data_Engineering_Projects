83567def352b9d7952c28832fed156b6aefeaf79
Quang Nguyen <quangn8888@gmail.com>
2023-07-14 16:40:47 -0700

    update

--- Google_Analytics_360_data/models/trans_per_device.sql
+++ Google_Analytics_360_data/models/trans_per_device.sql
@@ -1,13 +1,17 @@
 {{ config(materialized='table') }}
 
 SELECT
+
     date,
     browser,
     SUM (transactions) AS total_transactons
 FROM
+
     {{source('raw', 'ga_data_raw')}}
 GROUP BY
+
     date,
     browser
+
 ORDER BY
     total_transactons DESC
